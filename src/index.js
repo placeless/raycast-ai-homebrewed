7000d4bfb4cd0a728e36bd8bee7b30bce97c88ee
placeless <placeless@outlook.com>
2024-03-13 01:58:43 +0800

    feat: chat and translate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,55 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import CONFIG from './data/models.json' assert { type: 'json' };
+import { RaycastAIData, TranslationData } from './utils/helpers';
+import dispatch from './utils/detour';
+
+const JSONResponseHeaders = {
+	'Content-Type': 'application/json',
+	'Cache-Control': 'no-cache',
+	Connection: 'keep-alive',
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': 'Origin, X-Request-With, Content-Type, Accept',
+};
+
+const StreamResponseHeaders = {
+	'Content-Type': 'text/event-stream',
+	'Cache-Control': 'no-cache',
+	Connection: 'keep-alive',
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': 'Origin, X-Request-With, Content-Type, Accept',
+};
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(req, env) {
+		const url = new URL(req.url);
+		const path = url.pathname;
+		let data;
+		let agent;
+		let response;
+
+		switch (path) {
+			case '/api/v1/me':
+				// TODO: alter and download me.json
+				break;
+
+			case '/api/v1/ai/models':
+				// TODO: easy way to setup models.json?
+				break;
+
+			case '/api/v1/ai/chat_completions':
+				data = await RaycastAIData(req);
+				agent = dispatch(env, data);
+				response = await agent.onCompletion(data);
+				return new Response(response, { StreamResponseHeaders });
+
+			case '/api/v1/translations':
+				const model = CONFIG.models.find((item) => item.id === CONFIG.default_models.translate);
+				data = await TranslationData(req, model);
+				agent = dispatch(env, data);
+				response = await agent.onTranslation(data);
+				return new Response(JSON.stringify(response), { JSONResponseHeaders });
+
+			default:
+				break;
+		}
 	},
 };
